11b0ee5bb02044fa4b8eea529e2330e13ed0a7c2
zdegraaf <69164506+zdegraaf@users.noreply.github.com>
2021-10-28 15:22:13 -0400

    Parallel
    Replaced for loops with parellel to allow for parellel processing on a few of the slow points of the script.

--- meraki_org_update.sh
+++ meraki_org_update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx #Meraki Dashboard API Key
+output_folder="/usr/lib/zabbix/externalscripts/" #Should be the externalscripts folder of your Zabbix installation.
 #Grab basic Info for all Orginizations
 org_info=$(curl -L --request GET --url https://api.meraki.com/api/v0/organizations --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-Cisco-Meraki-API-Key: $api_key")
 #Creates full list of Org names
@@ -14,7 +15,7 @@ for i in "${orgs[@]}"; do
 	if [ ! -z "$org_id" ]; then
 		#Grabs the Network details via API calls
 		networks=$(curl -L --request GET --url https://api.meraki.com/api/v0/organizations/$org_id/networks --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-Cisco-Meraki-API-Key: $api_key")
-		devices=$(curl -L --request GET --url https://api.meraki.com/api/v0/organizations/$org_id/devices --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-Cisco-Meraki-API-Key: $api_key")
+		curl -L --request GET --url https://api.meraki.com/api/v0/organizations/$org_id/devices --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-Cisco-Meraki-API-Key: $api_key" > devcies.tmp
 		#Detect and output any errors with the pull of network statistics
 		echo $networks | jq '.errors | .[]' 2>/dev/null | sed 's/\"//g' | sed '/^[[:space:]]*$/d' > errors.tmp
 		error_count=$(wc -l errors.tmp | awk '{print $1}')
@@ -23,17 +24,17 @@ for i in "${orgs[@]}"; do
 		if [ $error_count -gt 0 ]; then
 			#Create Json object with error, then skip processing for the org in question
 			jo -p org_name="$i" error="$errors" >> json.tmp
-			
 		else
 			#Creates a list variables with the network details
 			IFS=$'\n' net_id=(`echo $networks | jq '.[] | .id' | sed 's/\"//g'`)
 			IFS=$'\n' net_name=(`echo $networks | jq '.[] | .name' | sed 's/\"//g'`)
-			c=0
-			#Loops through each network and adds the Meraki network name, organization name and organization ID to all device json objects. 
-			for a in "${net_id[@]}"; do
-				dev+=$(echo $devices | jq --arg var "$a" --arg var2 "${net_name[$c]}" --arg var3 "$i" --arg var4 "$org_id" '[.[] | select(.networkId == $var )] | map(. + {network_name: $var2}) | map(. + {org_name: $var3}) | map(. + {org_id: $var4})')
-				c=$c+1
-			done
+			#Declare merge function for use in parallel command 
+			merge(){
+			devices=$(cat devcies.tmp)
+			echo $devices | jq --arg var "$1" --arg var2 "$2" --arg var3 "$3" --arg var4 "$4" '[.[] | select(.networkId == $var )] | map(. + {network_name: $var2}) | map(. + {org_name: $var3}) | map(. + {org_id: $var4})' >> orgs.tmp
+			}
+			export -f merge
+			parallel -j 4 --link merge ::: "${net_id[@]}" ::: "${net_name[@]}" ::: $i ::: $org_id
 		fi
 	else
 		echo "Orginization $i was not found using your API key. Check to make sure the spelling is correct then try again"
@@ -43,6 +44,8 @@ done
 error_count=$(cat json.tmp | jq -s '.[] | .org_name' | wc -l)
 array=$(cat json.tmp | jq -s)
 rm json.tmp
-jo -p errors_count=$error_count errors="$array" > /docker/zabbix/server/externalscripts/meraki_errors.json 
+jo -p errors_count=$error_count errors="$array" > $output_folder"meraki_errors.json" 
 #Output finished json list
-echo $dev | jq > "/home/iserv/scripts/meraki/meraki_networks.json"
+cat orgs.tmp | jq > "meraki_networks.json"
+rm orgs.tmp
+rm devcies.tmp
--- meraki_status.sh
+++ meraki_status.sh
@@ -3,7 +3,9 @@ api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx #Meraki Dashboard API Key
 output_folder="/usr/lib/zabbix/externalscripts/" #Should be the externalscripts folder of your Zabbix installation, including trailing slash.
 IFS=$'\n' orgs=(`cat meraki_networks.json | jq '.[] | .org_name' | sort | uniq | sed 's/\"//g'`) #List of Meraki orginizations to monitor seperated by new lines. 
 #Increment through each org on the list
-for i in "${orgs[@]}"; do
+status(){
+	i=$1	
+	api_key=$2
 	#Grabs the Org ID
 	org_id=$(cat meraki_networks.json | jq --arg var1 "$i" '.[] |  select(.org_name == $var1 ).org_id' | sed 's/\"//g' | uniq)
 	if [ ! -z "$org_id" ]; then
@@ -17,10 +19,13 @@ for i in "${orgs[@]}"; do
 		#Places all of the values for a single netwok into a single string variable.
 		devices="${devices} ${devices_status} ${uplinks}"
 		#Merges the json objects together so that device details and statuses are within a single json object per device. 
-		dev+=$(echo $devices | jq -s '[.[] | .[]] | group_by(.serial) | map(add)')
+		echo $devices | jq -s '[.[] | .[]] | group_by(.serial) | map(add)' >> devices.tmp
 	else
 		echo "Orginization $i was not found using your API key. Check to make sure the spelling is correct then try again"
 	fi
-done
+}
+export -f status
+parallel -j 16 --link status ::: "${orgs[@]}" ::: $api_key
 #Output finished json list
-echo $dev | jq > $output_folder"meraki.json"
+cat devices.tmp | jq -s > $output_folder"meraki.json"
+rm devices.tmp
